cc64c7afb793754fa3d873750f0ae33fe34561fc
Alejandro Ramos <caramos336@gmail.com>
2023-06-17 17:40:11 -0300

    Ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+diccionario = {
+    "clave" : "valor",
+    "clave2" : 15
+}
+
+print(diccionario["clave2"]);
+
+# Escribe un programa en la consola de python que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, 
+# e imprima por pantalla la frase 
+# Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales. 
