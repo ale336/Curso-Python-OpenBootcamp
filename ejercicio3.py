ef891fb5613888ac391cbf5526a95dcfc1373ff9
Alejandro Ramos <caramos336@gmail.com>
2023-06-18 22:18:27 -0300

    Ejercicio4

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+# Escribe un programa que sea capaz de mostrar los números del 1 al 10 en orden inverso.
+#Si queremos hacer desde 100, cambiamos numero=101
+
+numero=11;
+
+for i in range(1,numero):
+    print(numero-i)
