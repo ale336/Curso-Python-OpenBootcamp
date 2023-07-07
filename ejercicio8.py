58e13e69e73c61dd85a0b7eaab2c738f3024b792
Alejandro Ramos <caramos336@gmail.com>
2023-07-07 01:19:34 -0300

    Ejercicio8-1-2

---
+++ ejercicio8.py
@@ -0,0 +1,32 @@
+# INPUT/OUTPUT EN PYTHON
+#  ACTUALMENTE SE UTILIXA LA PROPIEDAD DE LOS STRINGS "FORMAT"
+#  
+numero =511;
+texto ="quijote";
+
+print (f'El numero es {numero} y el texto es {texto}') #ES LA FORMA MAS HABITUAL , EJECUTAR PCODIGO PYTHON EN LAS {}
+
+# REPRESENTACIONES TEXTUALES DE UNA CLASE
+  #SOBRE CARGA DE METODOS
+
+# METODOS EN LAS CADENAS DE TEXTOS
+#  print(dir(''))  dir => nos muestra las propiedades de un objeto . Una cadena de texto vacia es un objeto.
+#  string.capitalize
+#  string.title
+#  string.count
+#  string.isdigit
+#  string.isalpha
+#  string.strip
+#  string.startswith
+#  
+# MANIPULACION DE FICHEROS
+#
+
+# EJERICIO 1
+#  En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, 
+#  lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos veces al archivo creado.
+
+
+# EJERCICIO 2
+#  En este segundo ejercicio, tendréis que crear un archivo py y dentro crearéis una clase Vehículo,
+#  haréis un objeto de ella, lo guardaréis en un archivo y luego lo cargamos.
