bbf359db328c7897a4331d0af2d77c4fa7963857
Alejandro Ramos <caramos336@gmail.com>
2023-07-17 18:25:03 -0300

    Ejercicio10-1-2

---
+++ ejercicio10.py
@@ -0,0 +1,15 @@
+#  INTERFAZ DE USUARIO
+# PYGTK
+# PYQT
+# TKINTER => LIBRERIA DE PYTHON
+
+# EJERCICIO 1
+
+#  En este ejercicio tenéis que crear una lista de RadioButton que muestre la opción que se ha seleccionado y que contenga un botón de 
+#  reinicio para que deje todo como al principio.
+#  Al principio no tiene que haber una opción seleccionada.
+#
+# EJERCICIO 2
+
+# En este segundo ejercicio, tendréis que crear una interfaz sencilla la cual debe de contener una lista de elementos 
+# seleccionables, también debe de tener un label con el texto que queráis.
